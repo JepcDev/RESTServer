9c49607e2ae60b88756ca47d3ea032424ab7ceeb
Jepc <joseenrique.pc21@gmail.com>
2023-03-05 22:47:58 -0400

    ADD: starting the project - RESTServer

---
+++ app.js
@@ -0,0 +1,13 @@
+
+require('dotenv').config();d
+
+const express = require('express')
+const app = express()
+
+app.get('/', function(req, res) {
+  res.send('Hello World')
+});
+
+app.listen(process.env.PORT, ()=> {
+  console.log('Servidor corriendo en el puerto: ', process.env.PORT);
+});
---
+++ info.md
@@ -0,0 +1,2 @@
+#INFO
+- El restserver aparte de permitirnos servir contenido estatico tambien nos permite tambien nos permite regresar informacion atravez de comunicacion por json vamos a poder crear endpoinst
