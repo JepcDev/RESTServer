644ecbc1be6873f864f83a2bcf7541afdf7cdd27
Jepc <joseenrique.pc21@gmail.com>
2023-03-21 23:30:31 -0400

    ADD: user model

---
+++ models/usuarios.js
@@ -0,0 +1,51 @@
+// mediante un modelo se haga toda la iteraccion contra nuestra collecion donde almacenaremos cada uno de nuestros usuarios de la app
+// como va lucir nuestra informacion en la base de datos
+// {
+//   nombre: 'asd',
+//   correo: 'asd@gmail.com',
+//   password: '1234', //encriptado
+//   rol: '1231231',
+//   estado: false, //no se elimanara de la base de datos solo se ponda false como inactivo
+//   google: false//si el usuario fue creado por google true y si fue creado por mi sistema de autencacion false
+// }
+
+const { Schema, model } = require('mongoose');
+
+// esto es un objeto literal
+const UsarioSchema = Schema({
+  nombre: {
+    type: String,
+    // la 1er posicion es de si es requerido y la 2da es un mensaje de error en caso de que no sea enviado
+    // aunque vamos hacer nuestra propias validaciones por que no se quiere llegar a la BD con informacion herronea , que la base trabaje con informacion veridica
+    required: [true, 'El nombre es obligatorio']
+  },
+  correo: {
+    type: String,
+    required: [true, 'El correo es obligatorio'],
+    unique: true,//mongo no va permitir insertar correos duplicados
+  },
+  password: {
+    type: String,
+    required: [true, 'La contraseña es obligatoria'],
+  },
+  img: {
+    type: String,
+  },
+  rol: {
+    type: String,
+    required: true,
+    enum: ['ADMIN_ROLE', 'USER_ROLE'],///valida que el rol solo sea de estos dos tipos
+  },
+  estado: {
+    type: Boolean,
+    default: true
+  },
+  google: {
+    type: Boolean,
+    default: false
+  },
+});
+
+
+// pide el nombre de no solo del modelo que esto ayudara para ponerle el nombre a la misma coleccion
+module.exports = model( 'Usuarios' );
